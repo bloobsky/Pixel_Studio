eb0854e84d57a0f957d333dd0ae342b22ddb2807
bloobsky <bloobsky@gmail.com>
2020-09-25 11:58:40 +0000

    Added View and Change template for FAQ app

--- faq/views.py
+++ faq/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Question
+from products.models import Category
 
 # Create your views here.
 
 def faq(request):
     """ A view to return the faq page """
     
-    return render(request, 'faq/faq.html')
+    
+    question = Question.objects.all()
+    categories = Category.objects.all()
+    webdev = Question.objects.filter(category='1')
+    context = {
+            'question': question,
+        }
+    return render(request, 'faq/faq.html', context)
